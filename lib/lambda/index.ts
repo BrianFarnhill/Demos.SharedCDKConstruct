cce4a0110ae63a9e19ad607fcaaa3492b01d1245
brfarn@amazon.com <brfarn@amazon.com>
2021-07-02 15:53:05 +1000

    Fixed typo in lambda code

--- lib/lambda/index.ts
+++ lib/lambda/index.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'Hello from a Lambda Function adn Brian was here!',
+    body: 'Hello from a Lambda Function and Brian was here!',
     statusCode: 200,
   };
 }
