8a952c862f28e29e12b6809af6cf2de48816e05c
brfarn@amazon.com <brfarn@amazon.com>
2021-07-02 11:42:55 +1000

    Fixed packaging issues

--- lib/construct.ts
+++ lib/construct.ts
@@ -1,5 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as path from "path";
 
 export interface ConstructProps {
   /**
@@ -11,11 +12,14 @@ export interface ConstructProps {
 }
 
 export class DemoFunction extends cdk.Construct {
+  
+  LambdaFunction: lambda.Function
+
   constructor(scope: cdk.Construct, id: string, props: ConstructProps = {}) {
     super(scope, id);
 
-    new lambda.Function(this, "DemoFunction", {
-      code: lambda.Code.fromAsset("./lambda"),
+    this.LambdaFunction = new lambda.Function(this, "DemoFunction", {
+      code: lambda.Code.fromAsset(path.resolve(__dirname, "./lambda")),
       handler: "index.handler",
       description: "An example function that comes from a shared CDK construct",
       runtime: lambda.Runtime.NODEJS_14_X,
