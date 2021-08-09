398514024c7136a6839b1c0236f7f31685bab5ea
brfarn@amazon.com <brfarn@amazon.com>
2021-08-09 14:44:02 +1000

    Updated to JSON body

--- lib/lambda/index.ts
+++ lib/lambda/index.ts
@@ -2,7 +2,9 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'Hello from a Lambda Function and Brian was here!',
+    body: JSON.stringify({
+      message: "This was last updated on 9/8/21 @ 2:42PM",
+    }),
     statusCode: 200,
   };
 }
