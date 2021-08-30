b3bf79e989f959bb0ab69e4bca5161173501a4ee
brfarn@amazon.com <brfarn@amazon.com>
2021-08-30 12:14:53 +1000

    Updated versions/times for demo

--- lib/lambda/index.ts
+++ lib/lambda/index.ts
@@ -3,7 +3,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
     body: JSON.stringify({
-      message: "This was last updated on 12/8/21 @ 3:23PM",
+      message: "This was last updated on 30/8/21 @ 12:05 PM",
     }),
     statusCode: 200,
   };
