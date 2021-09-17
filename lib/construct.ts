d45138878a057bcb2d61ff97e966c7c256de4747
brfarn@amazon.com <brfarn@amazon.com>
2021-09-17 14:59:24 +1000

    Updated for basic test demo

--- lib/construct.ts
+++ lib/construct.ts
@@ -10,19 +10,33 @@ export interface ConstructProps {
    * @default Duration.seconds(3)
    */
   timeout?: cdk.Duration;
+
+  /**
+   * Whether or not a timestamp should be added to the description to force a lambda update on each deployment
+   */
+  includeTimestamp?: boolean;
 }
 
 export class DemoFunction extends Construct {
-  
+
   LambdaFunction: lambda.Function
 
   constructor(scope: Construct, id: string, props: ConstructProps = {}) {
     super(scope, id);
 
+    if (props.timeout && props.timeout.toSeconds() > 300) {
+      throw new Error('This lambda must timeout in 5 minutes or less');
+    }
+
+    let description = 'An example function that comes from a shared CDK construct';
+    if (props.includeTimestamp === undefined || props.includeTimestamp === true) {
+      description += ` - last synthesised at ${Date.now().toString()}`;
+    }
+
     this.LambdaFunction = new lambda.Function(this, "DemoFunction", {
       code: lambda.Code.fromAsset(path.resolve(__dirname, "./lambda")),
       handler: "index.handler",
-      description: `An example function that comes from a shared CDK construct - last synthesised at ${Date.now().toString()}`,
+      description,
       runtime: lambda.Runtime.NODEJS_14_X,
       timeout: props.timeout,
     });
--- test/stack.test.ts
+++ test/stack.test.ts
@@ -1,7 +1,27 @@
 import { Template } from '@aws-cdk/assertions';
 import * as cdk from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
 import { DemoFunction } from '../lib/construct';
 
+
+/**
+ * A simple snapshot test
+ */
+ test('Matches SnapShot', () => {
+  const app = new cdk.App();
+
+  const stack = new cdk.Stack(app, "TestStack");
+  new DemoFunction(stack, "TestFunction", {
+    includeTimestamp: false,
+  });
+  const testStackOutput = app.synth().getStackArtifact("TestStack").template;
+
+  expect(Template.fromJSON(testStackOutput)).toMatchSnapshot();
+});
+
+/**
+ * A Simple fine-grained assertion
+ */
 test('Includes a lambda function in the output', () => {
   const app = new cdk.App();
 
@@ -14,4 +34,26 @@ test('Includes a lambda function in the output', () => {
       Handler: "index.handler",
     },
   });
+});
+
+/**
+ * Examples of basic validation tests
+ */
+test('Accepts a valid timeout value under 5 minutes', () => {
+  const app = new cdk.App();
+  expect(() => {
+    const stack = new cdk.Stack(app, "TestStack");
+    new DemoFunction(stack, "TestFunction", {
+      timeout: Duration.minutes(3),
+    })
+  }).not.toThrow();
+});
+test('Rejects an invalid timeout value over 5 minutes', () => {
+  const app = new cdk.App();
+  expect(() => {
+    const stack = new cdk.Stack(app, "TestStack");
+    new DemoFunction(stack, "TestFunction", {
+      timeout: Duration.minutes(10),
+    })
+  }).toThrow('This lambda must timeout in 5 minutes or less');
 });
